0b0045f302a7854a8fcd9515daa32fa9fbcce140
dhyaniv <vijay97dhyani@gmail.com>
2026-05-30 21:15:35 +0530

    feat: favicon & open graph img

--- app/layout.js
+++ app/layout.js
@@ -1,22 +1,31 @@
 import "./globals.css";
 
 export const metadata = {
-  title: "Peepify - Hand-Drawn Open Peeps Avatar Generator",
-  description: "Turn your photos into beautiful hand-drawn doodle caricatures in Pablo Stanley's signature Open Peeps illustration style!",
+  title: "Peepify - Hand Drawn Open Peeps Avatar Generator",
+  description: "Turn your photos into beautiful hand drawn doodle caricatures in Pablo Stanley's signature Open Peeps illustration style!",
   keywords: ["Open Peeps", "Doodle Generator", "Avatar Creator", "Pablo Stanley", "Vijay Dhyani", "Vertex AI", "Gemini 2.5 Flash", "Imagen 3", "Caricature Maker"],
   authors: [{ name: "Vijay Dhyani" }],
   openGraph: {
-    title: "Peepify - Hand-Drawn Open Peeps Avatar Generator",
-    description: "Turn your photos into beautiful hand-drawn doodle caricatures in Pablo Stanley's signature Open Peeps style!",
+    title: "Peepify - Hand Drawn Open Peeps Avatar Generator",
+    description: "Turn your photos into beautiful hand drawn doodle caricatures in Pablo Stanley's signature Open Peeps style!",
     url: "https://peppify.dhyani.site",
     siteName: "Peepify",
     type: "website",
     locale: "en_US",
+    images: [
+      {
+        url: "https://i.ibb.co/ymrwDC6C/image.png",
+        width: 1200,
+        height: 630,
+        alt: "Peepify - Hand Drawn Open Peeps Avatar Generator Preview",
+      },
+    ],
   },
   twitter: {
     card: "summary_large_image",
-    title: "Peepify - Hand-Drawn Open Peeps Avatar Generator",
-    description: "Turn your photos into beautiful hand-drawn doodle caricatures in Pablo Stanley's signature Open Peeps style!",
+    title: "Peepify - Hand Drawn Open Peeps Avatar Generator",
+    description: "Turn your photos into beautiful hand drawn doodle caricatures in Pablo Stanley's signature Open Peeps style!",
+    images: ["https://i.ibb.co/ymrwDC6C/image.png"],
   },
 };
 
